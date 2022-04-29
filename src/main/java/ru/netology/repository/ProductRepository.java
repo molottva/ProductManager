d573e4f8c6d2730ed76945454bad0b95a1239ab4
molottva <molottva@list.ru>
2022-04-29 23:07:18 +0300

    Reformat code

--- src/main/java/ru/netology/repository/ProductRepository.java
+++ src/main/java/ru/netology/repository/ProductRepository.java
@@ -12,7 +12,7 @@ public class ProductRepository {
     private Product[] products = new Product[0];
 
     //добавление продукта в конец массива
-    public void addProduct (Product addProduct) {
+    public void addProduct(Product addProduct) {
         Product[] tmp = new Product[products.length + 1];
         System.arraycopy(products, 0, tmp, 0, products.length);
         tmp[products.length] = addProduct;
@@ -20,15 +20,15 @@ public class ProductRepository {
     }
 
     //получить все сохранненные продукты в порядке добавления
-    public Product[] findAll () {
+    public Product[] findAll() {
         return getProducts();
     }
 
     //удалить по id
-    public void removeId (int id) {
+    public void removeId(int id) {
         Product[] tmp = new Product[products.length - 1];
         int i = 0;
-        for (Product product: products) {
+        for (Product product : products) {
             if (product.getId() != id) {
                 tmp[i] = product;
                 i++;
